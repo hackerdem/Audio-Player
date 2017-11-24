d701c3e49777092f6ee1475c37686a6cf188e7f1
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-11-24 20:33:39 +1100

    some gui function  and icons

--- view.py
+++ view.py
@@ -21,7 +21,20 @@ class View:
         self.create_list_box()
         self.create_bottom_frame()
         
+    def create_top_display(self):
+        frame=tk.Frame(self.root)
+        glass_frame_image=tk.PhotoImage(file='../icons/glass_frame.gif')
+        self.canvas=tk.Canvas(frame,width=370,height=90)
+        self.canvas.image=glass_frame_image
+        self.canvas.grid(row=1)
+        self.console=self.canvas.create_image(0,10,anchor=tk.NW,image=glass_frame_image)
+        self.clock=self.canvas.create_text(125,68,anchor=tk.W,fill='#CBE4F6',text='00:00')
+        self.track_lenght_text=self.canvas.create_text(167,68,anchor=tk.W,fill='#CBE4F6',text="of 00:00")
+        self.track_name=self.canvas.create_text(50,35,anchor=tk.W,fill='#9CEDAC',text='\"Currently playing: none \"')
+        frame.grid(row=1,pady=1,padx=0)
     
+    def create_button_frame(self):
+        pass
 
 
 if __name__=='__main__':
