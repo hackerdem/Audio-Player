c74556e96405f8ea06bc013f5bb95cf29a2f8f98
Erdem Yilmaz <hackerdem@users.noreply.github.com>
2017-11-30 22:05:39 +1100

    interface improvements

--- view.py
+++ view.py
@@ -8,6 +8,7 @@ import player
 AUDIO_PLAYER_NAME="Audio Player"
 
 class View:
+    loop_choices=[("No Loop",1),("Loop Current",2),("Loop All",3)]
     def __init__(self,rot,model,player):
         self.root=root
         self.model=model
@@ -34,14 +35,34 @@ class View:
         frame.grid(row=1,pady=1,padx=0)
     
     def create_button_frame(self):
-        pass
-
+        frame=tk.Frame(self.root)
+        previous_track_icon=tk.PhotoImage(file='../icons/previos_track.gif')
+        previous_track_button=tk.Button(frame,
+                                        image=previous_track_icon,borderwidth=0,padx=0,command=self.on_previous_track_button_clicked)
+        previous_track_button.image=previous_track_icon
+        previous_track_button.grid(row=3,column=1,sticky='w')
+        rewind_icon=tk.PhotoImage(file='../icons/rewind.gif')
+        rewind_button=tk.Button(frame,
+                                image=previous_track_icon,borderwidth=0,padx=0,command=self.on_rewind_button_clicked)
+        self.play_icon=tk.PhotoImage(file='../icons/play.gif')
+        self.play_icon=tk.PhotoImage(file='../icons/stop.gif')
+        self.play_stop_button=tk.Button(frame,
+                                        image=self.play_icon,borderwidth=0,padx=0,command=self.on_play_stop_button_clicked)    
+        self.play_stop_button.image=self.play_icon
+        self.play_stop_button.grid(row=3,column=3)
+        
+        self.pause_icon=tk.PhotoImage(file='../icons/pause.gif')
+        pause_unpause_button=tk.Button(frame,
+                                       image=spause_icon,borderwidth=0,padx=0,command=self.on_pause_unpause_button_clicked)    
+        pause_unpause_button.image=pause_icon
+        pause_unpause_button.grid(row=3,column=4)        
+        
 
 if __name__=='__main__':
     root=Tk()
     root.resizable(width=False,height=False)
     player=player()
     model=model.Model()
-    app=View(roor,model,player)
+    app=View(root,model,player)
     root.mainloop()
     
